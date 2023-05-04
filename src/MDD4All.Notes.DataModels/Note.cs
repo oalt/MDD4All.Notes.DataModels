962e165cce5a08be38b68fb087fc050213295460
Oliver Alt <oa@lacheweg21.de>
2023-05-04 10:42:53 +0200

    Initial code commit

---
+++ src/MDD4All.Notes.DataModels/Note.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Copyright (c) MDD4All.de, Dr. Oliver Alt
+ */
+using Newtonsoft.Json;
+using System;
+
+namespace MDD4All.Notes.DataModels
+{
+    public class Note
+    {
+        [JsonProperty("guid")]
+        public string GUID { get; set; } = Guid.NewGuid().ToString();
+
+        [JsonProperty("title")]
+        public string Title { get; set; } = "";
+
+        [JsonProperty("description")]
+        public string Description { get; set; } = "";
+    }
+}
---
+++ src/MDD4All.Notes.DataModels/Notes.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Copyright (c) MDD4All.de, Dr. Oliver Alt
+ */
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace MDD4All.Notes.DataModels
+{
+    public class Notes
+    {
+        [XmlElement("Note")]
+        [JsonProperty("notes")]
+        public List<Note> NoteElements { get; set; } = new List<Note>();
+    }
+}
